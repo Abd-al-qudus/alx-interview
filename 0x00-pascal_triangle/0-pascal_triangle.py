dfb1e80f16f97230693db33c3202effa272ad5c9
Abd-al-qudus <Abd-al-qudus>
2023-07-01 15:10:30 +0000

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+"""
+    generates the pascal triangle of n
+    """
+
+ 
+def pascal_triangle(n):
+    '''generates the nth term of the pascal triangle'''
+    triangle = [[1], [1, 1]]
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [[1]]
+    elif n == 2:
+        return triangle
+    else:
+        for i in range(2, n):
+            last = triangle[-1]
+            new = []
+            new.insert(0, 1)
+            for j in range(1, len(last)):
+                sum = last[i - 1] + last[i]
+                new.insert(len(new), sum)
+            new.insert(len(new), 1)
+            triangle.insert(i, new)
+
+        return triangle
