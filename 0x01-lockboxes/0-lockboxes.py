a9aff3d247610d732e4e4fd5e3f1becad0902922
Abd-al-qudus <jimohqudus86@gmail.com>
2023-07-04 14:58:02 +0100

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+"""
+lockboxes returns true when all boxes can be unlocked and vice versa
+"""
+
+
+def canUnlockAll(boxes):
+    status = False
+    if boxes[0]:
+        lck_keys = boxes[0]
+        for key in lck_keys:
+            if boxes[key] and not len(boxes[key]) == 0:
+                status = True
+            else:
+                status = False
+        return status
