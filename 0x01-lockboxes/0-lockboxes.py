6799f28c5b3d9f7984edc6338364fd814b1f195d
Abd-al-qudus <jimohqudus86@gmail.com>
2023-07-06 16:11:21 +0100

    fixed BFS algorithm

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""
-lockboxes returns true when all boxes can be unlocked and vice versa
-"""
+
+from collections import deque
+
+
+""" lockboxes """
 
 
 def canUnlockAll(boxes):
-    """ try to unlock all boxes """
-    status = False
-    if boxes[0]:
-        lck_keys = boxes[0]
-        for key in lck_keys:
-            if boxes[key]:
-                status = True
-            else:
-                status = False
-        return status
+    """ try unlocking the boxes by unlocking box 1 """
+    opened_boxes = set([0])
+    queue = deque([0])
+    while queue:
+        box = queue.popleft()
+        keys = boxes[box]
+        for key in keys:
+            if key not in opened_boxes:
+                opened_boxes.add(key)
+                queue.append(key)
+
+    return len(opened_boxes) == len(boxes)
