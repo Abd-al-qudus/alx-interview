299c540fb69c3c909db76b2551ad46f236b08a43
Abd-al-qudus <jimohqudus86@gmail.com>
2023-07-06 23:20:02 +0100

    fixed BFS

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-""" try unlocking all boxes """
 
-if __name__ == '__main__':
-    from collections import deque
+"""lockboxes"""
 
-    def canUnlockAll(boxes):
-        """ check a key to unlock the boxes"""
-        opened_boxes = set([0])
-        queue = deque([0])
-        while queue:
-            box = queue.popleft()
-            keys = boxes[box]
-            for key in keys:
-                if key not in opened_boxes:
-                    opened_boxes.add(key)
-                    queue.append(key)
-        return len(opened_boxes) == len(boxes)
+
+def canUnlockAll(boxes):
+    """try unlocking all boxes"""
+    free_keys = set()
+    box_id = 0
+
+    opened_boxes = []
+    num_boxes = len(boxes)
+    while box_id < num_boxes:
+        old_box_id = box_id
+        opened_boxes.append(box_id)
+        free_keys.update(boxes[box_id])
+        for key in free_keys:
+            if key != 0 and key < num_boxes and key not in opened_boxes:
+                box_id = key
+                break
+        if old_box_id == box_id:
+            break
+        continue
+    for box_id in range(num_boxes):
+        if box_id not in opened_boxes and box_id != 0:
+            return False
+    return True
