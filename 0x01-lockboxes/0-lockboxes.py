f5ad275f44214c0d9ebddf54da0b2f2bea92bbcd
Abd-al-qudus <jimohqudus86@gmail.com>
2023-07-06 12:34:08 +0100

    added docs to function

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,6 +5,7 @@ lockboxes returns true when all boxes can be unlocked and vice versa
 
 
 def canUnlockAll(boxes):
+    """ try to unlock all boxes """
     status = False
     if boxes[0]:
         lck_keys = boxes[0]
