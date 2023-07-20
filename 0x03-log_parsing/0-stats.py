a9c7b6ba2ea6ed211696e2691b29c0fb4b6f824c
Abd-al-qudus <Abd-al-qudus>
2023-07-20 23:40:33 +0000

    fixed unknown error

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -13,37 +13,36 @@ def print_status_codes(status_codes: dict, total_size: int) -> None:
             print("{}: {}".format(code, count))
 
 
-if __name__ == "__main__":
-    regex_format = (r'^(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] '
-                    r'"GET /projects/\d+ HTTP/1\.1" (\d+) (\d+)$')
+regex_format = (r'^(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] '
+                r'"GET /projects/\d+ HTTP/1\.1" (\d+) (\d+)$')
 
-    line_count = 0
-    total_file_size = 0
-    status = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0
-    }
+line_count = 0
+total_file_size = 0
+status = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
 
-    try:
-        for line in sys.stdin:
-            stdin_entry = line.strip()
-            get_match = re.match(regex_format, stdin_entry)
-            if get_match:
-                status_code = get_match.group(3)
-                file_size = get_match.group(4)
-                total_file_size += int(file_size)
-                line_count += 1
-                for codes in status.keys():
-                    if codes == status_code:
-                        if int(status_code):
-                            status[codes] += 1
-            if line_count % 10 == 0:
-                print_status_codes(status, total_file_size)
-    except KeyboardInterrupt:
-        print_status_codes(status, total_file_size)
+try:
+    for line in sys.stdin:
+        stdin_entry = line.strip()
+        get_match = re.match(regex_format, stdin_entry)
+        if get_match:
+            status_code = get_match.group(3)
+            file_size = get_match.group(4)
+            total_file_size += int(file_size)
+            line_count += 1
+            for codes in status.keys():
+                if codes == status_code:
+                    if int(status_code):
+                        status[codes] += 1
+        if line_count % 10 == 0:
+            print_status_codes(status, total_file_size)
+finally:
+    print_status_codes(status, total_file_size)
