e7ae609ebc9e79949e928f6c4b97696dc0224f8e
Abd-al-qudus <Abd-al-qudus>
2023-08-25 09:41:26 +0000

    rotate 2 dimensional matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+"""rotate a 2 dimensional matrix by 90 degrees"""
+
+
+def rotate_2d_matrix(matrix):
+    """rotate the matrix"""
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
